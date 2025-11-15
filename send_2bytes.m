b24cdda1c22822c9de7e31c7000665b45d5b22c6
Prof-Carter <106240356+Prof-Carter@users.noreply.github.com>
2025-11-15 21:36:26 +0900

    Add files via upload

---
+++ send_2bytes.m
@@ -0,0 +1,17 @@
+clear
+
+m5 = ble("Arduino");
+
+ServiceUUID = "fce70352-c544-48f4-9760-7568263b7922";
+characteristicUID = "b570118e-df2f-4545-9cf9-eff837991f54";
+
+data_c = characteristic(m5, ServiceUUID, characteristicUID);
+
+while 1
+    ref = input("ref = ");   % -32768 ～ 32767 の値を入力
+    
+    % --- int16 を 2 バイトの uint8 に変換（ビットパターン保持）---
+    bytesToSend = typecast(int16(ref), 'uint8');  % 1x2 の uint8 ベクトルになる
+    
+    write(data_c, bytesToSend);
+end
