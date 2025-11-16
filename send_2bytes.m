f6a78d294478a721c6859624ac8da1d3834c0652
Prof-Carter <106240356+Prof-Carter@users.noreply.github.com>
2025-11-16 18:07:54 +0900

    Add files via upload

--- send_2bytes.m
+++ send_2bytes.m
@@ -2,10 +2,11 @@ clear
 
 m5 = ble("Arduino");
 
-ServiceUUID = "fce70352-c544-48f4-9760-7568263b7922";
-characteristicUID = "b570118e-df2f-4545-9cf9-eff837991f54";
+% UUID の設定
+Service        = "fce70352-c544-48f4-9760-7568263b7922";
+Characteristic = "b570118e-df2f-4545-9cf9-eff837991f54";
 
-data_c = characteristic(m5, ServiceUUID, characteristicUID);
+data_c = characteristic(m5, Service, Characteristic);
 
 while 1
     ref = input("ref = ");   % -32768 ～ 32767 の値を入力
